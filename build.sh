30ba3c63f26027102e75a47335622db1848d6693
NE <NENightElves@qq.com>
2026-08-09 07:49:19 +0000

    update build

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 . ./config.sh
 
-rm -r dist
+if [ -d dist ]; then
+    rm -r dist
+fi
 mkdir dist
 
 for d in ${tasks}; do
     npx @marp-team/marp-cli ${d}/${d}.md -o dist/${d}.html --html --theme ${theme}
+    if [ -d ${d}/${d}-assets ]; then
+        cp -r ${d}/${d}-assets dist
+    fi
 done
